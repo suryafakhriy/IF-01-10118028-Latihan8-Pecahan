8b08772c62f4338d18fa5b3fb6527417d27642ce
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-09-26 21:24:42 +0700

    Add files via upload

---
+++ IF-01-10118028-Latihan8-Pecahan/src/pkgif/pkg01/pkg10118028/latihan8/pecahan/IF0110118028Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg01.pkg10118028.latihan8.pecahan;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Pecahan
+ */
+public class IF0110118028Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float = " + f);
+        System.out.println("Double = " + d);
+    }
+    
+}
